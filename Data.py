f80df3730892860584e8479f56213ca1d320b2fd
AkumaHiNoTori <omar.magdyds@gmail.com>
2024-10-19 01:04:24 +0300

    the last last
    XDD read last description pleasee

--- Data.py
+++ Data.py
@@ -4,9 +4,9 @@ import math
 import numpy as np
 
 numdrones = 2   #depends on the number of drones
-maxpdist = 3 #depends on the controller accuracy
+maxpdist = 4 #depends on the controller accuracy 
 gridsize = 10 #depends on the size of the grid
-numtrackp = math.ceil((((((gridsize-0)**2)+((gridsize-0)**2)+((gridsize-0)**2))**0.5)/maxpdist) + 1)*5
+numtrackp = math.ceil((((((gridsize-0)**2)+((gridsize-0)**2)+((gridsize-0)**2))**0.5)/maxpdist) + 1)*6
 distDrone = []
 dsafe = 2
 amax = 75 *numpy.pi/180
