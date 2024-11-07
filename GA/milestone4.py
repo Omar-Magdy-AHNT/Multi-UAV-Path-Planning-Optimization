40f87d5a8242ba088f575aa8500c0de8e4911627
AkumaHiNoTori <omar.magdyds@gmail.com>
2024-11-07 06:11:40 +0200

    ga

--- GA/milestone4.py
+++ GA/milestone4.py
@@ -37,19 +37,35 @@ def crossover():
     for i in range(numchildren//2):
         child1 = []
         child2 = []
-        flag = True
-        while flag:
-            for j in range(numdrones):
-                for k in range(numtrackp+2):
-                    if k == 0:
-                        children[i][k] = startpoint[j]
-                        continue
-                    if k == numtrackp+1:
-                        children[i][k] = endpoint[j]
-                        continue
-                    child1[k] = (alpha*parents[0][k]) - (1-alpha)*parents[i+1][k]
-                    child2[k] = (alpha*parents[i+1][k])- (1-alpha)*parents[0][k]
-            check()
+        for j in range(numdrones):
+            for k in range(numtrackp+2):
+                f1 = True
+                f2 = True
+                if k == 0:
+                    children[i][k] = startpoint[j]
+                    continue
+                if k == numtrackp+1:
+                    children[i][k] = endpoint[j]
+                    continue
+                while f1:
+                    alpha = random.uniform(0.2,0.9)
+                    x1 = (alpha*parents[0][k]) - (1-alpha)*parents[i+1][k]
+                    y1 = (alpha*parents[0][k]) - (1-alpha)*parents[i+1][k]
+                    z1 = (alpha*parents[0][k]) - (1-alpha)*parents[i+1][k]
+                    c1 = (x1,y1,z1)
+                    f1 = check(c1,child1)
+                    if f1 == False:
+                        child1.append((x1,y1,z1))
+
+                while f2:
+                    x2 = (alpha*parents[i+1][k])- (1-alpha)*parents[0][k]
+                    y2 = (alpha*parents[i+1][k])- (1-alpha)*parents[0][k]
+                    z2 = (alpha*parents[i+1][k])- (1-alpha)*parents[0][k]
+                    c2 = (x2,y2,z2)
+                    f2 = check(c2,child2)
+                    if f2 == False:
+                        child2.append((x2,y2,z2))
+
         children.append(child1)
         children.append(child2)
     if numchildren % 2 == 1:
@@ -83,9 +99,14 @@ def mutation():
                 if k == numtrackp+1:
                     children[i][k] = endpoint[j]
                     continue
-                children[i][k] = mutants[k] + random.uniform(-1,1)
+                x1 = mutants[k] + random.uniform(-1,1)
+                y1 = mutants[k] + random.uniform(-1,1)
+                z1 = mutants[k] + random.uniform(-1,1)
+                if check((x1,y1,z1),children[i]) == False:
+                    children[i][k] = (x1,y1,z1)
     for i in range(nummutants):
         children.extend(mutants)
 
-def check():
+def check(p1,a1):
+    #takes 2 points and checks them
 createmap()
